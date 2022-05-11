918e11fe842886f447b78e86ce0932bd7431a77d
Martins <martins.zeltins@arkbauer.com>
2022-05-11 09:45:16 +0300

    Initial commit

---
+++ shared/src/components/MySharedComponentButton.vue
@@ -0,0 +1,11 @@
+<template>
+    Button with 3rd party tooltip component <br><br>
+
+    <div>
+        <button
+            class="bg-gray-200 rounded px-2 py-1"
+            v-tooltip="'You have new messages.'">
+            Button
+        </button>
+    </div>
+</template>
